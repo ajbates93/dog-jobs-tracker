2cad77109946b34ed16ef878028106dee47e0228
Alex Bates <alex@hciyork.com>
2021-10-22 15:35:03 +0100

    Add some styling and basic ui

---
+++ src/components/Nav.vue
@@ -0,0 +1,19 @@
+<template>
+  <header class="border-b">
+    <div class="p-5">
+      <nav>
+        <ul class="flex flex-wrap items-center justify-center text-base">
+          <li class="mr-5 text-sm font-semibold hover:text-gray-800"><router-link to="/">Home</router-link></li>
+          <li class="mr-5 text-sm font-semibold hover:text-gray-800"><router-link to="about">About</router-link></li>
+          <li class="mr-5 ml-auto text-sm font-semibold hover:text-gray-800"><router-link to="/login">Login</router-link></li>
+        </ul>
+      </nav>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Nav"
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,15 @@
 <template>
   <div class="home">
-    To-Do:
-    <ul>
-      <li>To-Do 1</li>
-      <li>To-Do 2</li>
-      <li>To-Do 3</li>
-    </ul>
+    <h1 class="text-center text-4xl my-10">Dog Jobs Tracker</h1>
+    <p class="text-lg text-gray-500 text-center my-5">Keep track of when your dog has had a jab or flea/worm treatment.</p>
+    <article class="p-10 border m-10 w-1/3 mx-auto text-center rounded-2xl">
+      <ul>
+        <li>To-Do 1</li>
+        <li>To-Do 2</li>
+        <li>To-Do 3</li>
+      </ul>
+      <input type="text" class="border" />
+    </article>
   </div>
 </template>
 
