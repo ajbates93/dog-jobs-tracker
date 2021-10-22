db0070a51dd784342481013ee26e84a428642f99
Alex Bates <alex@hciyork.com>
2021-10-22 15:12:57 +0100

    moved all items up one level

---
+++ src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">
+    To-Do:
+    <ul>
+      <li>To-Do 1</li>
+      <li>To-Do 2</li>
+      <li>To-Do 3</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data() {
+
+  },
+  computed: {
+
+  }
+}
+</script>
